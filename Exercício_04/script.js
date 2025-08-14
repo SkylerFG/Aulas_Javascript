a96456f4852e18ffa461fcc142c3a2a8d3915baa
SkylerFG <contato.lipegarcia@gmail.com>
2025-08-14 10:53:59 -0300

    Add files via upload

---
+++ Exercício_04/script.js
@@ -0,0 +1,6 @@
+function proximafase() {
+    document.getElementById("foto").src = "images/1487447216_image.png";
+    const som = new Audio("audio/Dark Souls III OST - Vordt of the Boreal Valley (Phase 2 only).mp3");
+    som.play();
+    som.volume = 0.3;
+}
